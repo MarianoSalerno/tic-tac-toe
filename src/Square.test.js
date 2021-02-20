18aa25bbc2613a2608fc9daf6cb331691d250388
Mariano Salerno <mariano.salerno@toptal.com>
2021-02-20 16:08:12 -0300

    added square test

--- src/Square.test.js
+++ src/Square.test.js
@@ -1,3 +1,14 @@
+import { mount } from 'enzyme'
+import Square from './Square'
+
+const mockOnCLick = jest.fn()
+
 it('renders properly', () => {
-  
+  const squareValue = 'X'
+  const wrapper = mount(<Square value={squareValue} onClick={mockOnCLick} />)
+  const button = wrapper.find('button')
+
+  expect(button.text()).toEqual(squareValue)
+  button.simulate('click')
+  expect(mockOnCLick).toHaveBeenCalled()
 })
